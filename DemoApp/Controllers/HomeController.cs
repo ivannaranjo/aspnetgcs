209b72b87975e0634bfe90aee0a2d8bb42b5f62a
Ivan Naranjo <ivann@google.com>
2016-06-03 14:19:06 +0000

    List buckets, files within a bucket.

--- DemoApp/Controllers/HomeController.cs
+++ DemoApp/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Google.Storage.V1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +10,22 @@ namespace DemoApp.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private readonly Lazy<Task<StorageClient>> _storageClient = new Lazy<Task<StorageClient>>(() => StorageClient.CreateAsync());
+       
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var client = await _storageClient.Value;
+            var buckets = await client.ListAllBucketsAsync("my-demo-1331");
+
+            return View(buckets);
+        }
+
+        public async Task<ActionResult> Bucket(string name)
+        {
+            var client = await _storageClient.Value;
+            var files = await client.ListAllObjectsAsync(name, null);
+
+            return View(files);
         }
 
         public ActionResult About()
